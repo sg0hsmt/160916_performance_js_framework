ca7b1bd7e480442ef77fed37e1a79d0a5bf3b0b4
Shigeo Hashimoto <sg0hsmt@gmail.com>
2021-03-21 18:43:49 +0900

    Use template based functional component

--- vue-svg-functional/src/components/ParticleComponent.vue
+++ vue-svg-functional/src/components/ParticleComponent.vue
@@ -1,24 +1,10 @@
-<script>
-export default {
-  name: "ParticleComponent",
-  functional: true,
-  props: {
-    particle: {
-      type: Object,
-      required: true,
-    },
-  },
-  render(createElement, context) {
-    return createElement("g", null, [
-      createElement("rect", {
-        attrs: {
-          x: context.props.particle.displayX,
-          y: context.props.particle.displayY,
-          width: 2,
-          height: 2,
-        },
-      }),
-    ]);
-  },
-};
-</script>
+<template functional>
+  <g>
+    <rect
+      :x="props.particle.displayX"
+      :y="props.particle.displayY"
+      width="2"
+      height="2"
+    ></rect>
+  </g>
+</template>
