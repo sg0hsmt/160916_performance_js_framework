5e437ae73a17f9e0cb95da9201bb7a9bdfbf6206
Shigeo Hashimoto <sg0hsmt@gmail.com>
2021-08-29 21:00:48 +0900

    Add vue3.2-svg-composition

---
+++ vue3.2-svg-composition/src/components/ParticleComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <g>
+    <rect :x="x" :y="y" width="2" height="2" />
+  </g>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    x: {
+      type: Number,
+      required: true,
+    },
+    y: {
+      type: Number,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
