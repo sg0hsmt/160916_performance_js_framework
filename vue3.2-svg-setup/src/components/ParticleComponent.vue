5876b5231d6a209a68e4dfd5f02403cac144ffde
Shigeo Hashimoto <sg0hsmt@gmail.com>
2021-08-29 22:44:58 +0900

    Add vue3.2-svg-setup

---
+++ vue3.2-svg-setup/src/components/ParticleComponent.vue
@@ -0,0 +1,22 @@
+<script setup>
+// eslint-disable-next-line no-unused-vars, no-undef
+const props = defineProps({
+  x: {
+    type: Number,
+    required: true,
+  },
+  y: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <g>
+    <rect :x="x" :y="y" width="2" height="2" />
+  </g>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
